f628d8844243b902e1fac5e4f581f7ba17908a86
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-01 16:33:33 +0200

    Fixing code and Add README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+## deno-crud-api
+
+Classical CRUD API implemented in deno.
+For run and test this API, type this command in your shell:
+```bash
+deno run --allow-read --allow-net api.ts
+```
+
+For semplicity, I create a simple script that execute this command
+
+## Testing API
+
--- api.ts
+++ api.ts
@@ -1,41 +1,45 @@
 import { Application } from 'https://deno.land/x/abc@v1.0.0-rc10/mod.ts';
 import { v4 } from 'https://deno.land/std/uuid/mod.ts';
 
-let clients = [
-	{ id: '1', firstName: 'bart', lastName: 'zalewski' },
-	{ id: '2', firstName: 'long', lastName: 'shong' },
-	{ id: '3', firstName: 'joe', lastName: 'harrison' },
+let users = [
+	{ id: '1', firstName: 'bart', lastName: 'zalewski', username: "b.zalewski", email: "bart.zalewski@gmail.com"},
+	{ id: '2', firstName: 'long', lastName: 'shong', username: "l.shong", email: "long.shong@gmail.com"},
+	{ id: '3', firstName: 'joe', lastName: 'harrison', username: "j.harrison", email: "joe.harrison@gmail.com"},
 ];
 
-const getClients = (ctx: any) => ctx.json(clients);
+const getUsers = (context: any) => context.json(users);
 
-// GET CLIENTS
-const getClient = (ctx: any) => {
-	const { id } = ctx.params;
-	const client = clients.find((c) => c.id === id);
-	client ? ctx.json(client) : ctx.string('No client with that ID. ');
+// Method for getting clients
+const getUser = (context: any) => {
+	const { id } = context.params;
+	const user = users.find((user) => user.id === id);
+	user ? context.json(user) : context.string('No user with that ID. ');
 };
 
-// ADD A CLIENT
-const addClient = async (ctx: any) => {
-	const { firstName, lastName } = await ctx.body();
+// Method for adding a user
+const addUser = async (context: any) => {
+	const { firstName, lastName, username, email } = await context.body();
 	const id = v4.generate();
-	const client = { id, firstName, lastName };
-	clients.push(client);
-	return ctx.json(client);
+	const user = { id, firstName, lastName, username, email};
+	users.push(user);
+	return context.json(user);
 }
 
-// REMOVE CLIENT
-const removeClient = (ctx: any) => {
-	const { id } = ctx.params;
-	const client = clients.find((c) => c.id === id);
-	if (client) {
-		clients = clients.filter((c) => c.id !== id);
-		return ctx.json(client)
+// Method for removing a user
+const removeUser = (context: any) => {
+	const { id } = context.params;
+	const user = users.find((user) => user.id === id);
+	if (user) {
+		users = users.filter((user) => user.id !== id);
+		return context.json(user)
 	}
-	return ctx.string('No client with that ID. ');
+	return context.string('No client with that ID. ');
 };
 
 const app = new Application();
-app.get('/clients', getClients).get('clients/:id', getClient).post('/clients', addClient).delete('/clients/:id', removeClient).start({ port: 5000 });
+app.get('/clients', getUsers)
+	.get('clients/:id', getUser)
+	.post('/clients', addUser)
+	.delete('/clients/:id', removeUser)
+	.start({ port: 5000 });
 
