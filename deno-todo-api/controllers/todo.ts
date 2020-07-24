0862faae70c97190c6b6cea8e1c4a308d2132684
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-07-24 20:22:43 +0200

    Refactoring code

--- deno-todo-api/controllers/todo.ts
+++ deno-todo-api/controllers/todo.ts
@@ -6,9 +6,7 @@ import todos from "../data/todos.ts";
 export default {
   getAll: ({ response } : { response: any }) => {
     response.status = 200;
-    response.body = {
-      data: todos
-    };
+    response.body = { data: todos };
     console.log(todos);
   },
 
@@ -16,9 +14,7 @@ export default {
     const body = await request.body();
     if (!request.hasBody) {
       response.status = 400;
-      response.body = {
-        message: "Please provide the task detail and  status",
-      };
+      response.body = { message: "Please provide the task detail and  status", };
       return;
     }
     let newTodo: Todo = {
@@ -27,9 +23,7 @@ export default {
       done: false,
     };
     let data = [...todos, newTodo];
-    response.body = {
-      data
-    };
+    response.body = { data };
     todos.push(newTodo);
     console.log(JSON.stringify(todos));
     response.body = { message: 'OK' }
@@ -37,50 +31,35 @@ export default {
   },
 
   getById: ({ params, response }: { params: { id: string }; response: any }, ) => {
-    const todo: Todo | undefined = todos.find((t) => {
-      return t.id === params.id;
-    });
+    const todo: Todo | undefined = todos.find((t) => { return t.id === params.id; });
     if (!todo) {
       response.status = 404;
-      response.body = {
-        message: "No task found related to given ID",
-      };
+      response.body = { message: "No task found related to given ID", };
       return;
     }
     response.status = 200;
-    response.body = {
-      data: todo
-    };
+    response.body = { data: todo };
   },
 
   update: async ({ params, request, response }: { params: { id: string }, request: any, response: any, }, ) => {
     const todo: Todo | undefined = todos.find((t) => t.id === params.id);
     if (!todo) {
       response.status = 404;
-      response.body = {
-        message: "No todo found",
-      };
+      response.body = { message: "No todo found", };
       return;
     }
 
     const body = await request.body();
     const updatedData: { task?: string; done?: boolean } = body.value;
 
-    let newTodos = todos.map((t) => {
-      return t.id === params.id ? { ...t, ...updatedData } : t;
-    });
+    let newTodos = todos.map((t) => { return t.id === params.id ? { ...t, ...updatedData } : t; });
     response.status = 200;
-    response.body = {
-      data: newTodos,
-    };
+    response.body = { data: newTodos, };
   },
 
   delete: ({ params, response }: { params: { id: string }; response: any }, ) => {
     const allTodos = todos.filter((t) => t.id !== params.id);
     response.status = 200;
-    response.body = {
-      data: allTodos,
-    };
+    response.body = { data: allTodos, };
   },
 };
-
